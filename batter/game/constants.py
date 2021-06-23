48c1f246b175d5a7b3c9a55fb44ea494608603af
Sombreroboy <83668223+Sombreroboy@users.noreply.github.com>
2021-06-23 11:19:12 -0600

    movement and then its done

--- batter/game/constants.py
+++ batter/game/constants.py
@@ -2,7 +2,7 @@ import os
 
 MAX_X = 80
 MAX_Y = 25
-FRAME_LENGTH = 0.05
+FRAME_LENGTH = 0.13
 PATH = os.path.dirname(os.path.abspath(__file__))
 #MESSAGES = open(PATH + "/messages.txt").read().splitlines()
 bricks = 30
